19aa7b4d25185ca869b6c08450bd448032f9f937
Deo Evander <116868069+deobit@users.noreply.github.com>
2025-09-13 04:40:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = async () => {
     const conn = await mysql.createConnection({
         user: 'root',
-        password: 'root',
+        password: 'thisissecret',
         database: 'test_auth',
         host: '127.0.0.1',
         port: 3306
